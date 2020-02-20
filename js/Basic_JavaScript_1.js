34f59987071f5f81b90ca9af99a979875fea939b
unknown <smithcolemen@gmail.com>
2020-02-19 16:31:43 -0800

    Did more assignments

--- js/Basic_JavaScript_1.js
+++ js/Basic_JavaScript_1.js
@@ -1,3 +1,30 @@
-window.alert('Hello World!')
+var X = 'Hello World!';
+document.write(X);
 
-document.write('Hello World!')
+window.alert('Hello World!');
+
+document.write('Hello World!');
+
+var Assignment = "This is code for an assignment";
+
+window.alert(Assignment);
+
+document.write("I don't really know what to do, so I'll just say \"Ye I dunno bro\" " + Assignment);
+
+var assignmentTwo = "Ye idk Bro" + " See you later loser";
+
+document.write(assignmentTwo);
+
+var me = 'Colemen',
+    gf = 'Kylee',
+    cat = 'boots';
+
+document.write(gf + cat + me);
+
+var me = me.fontcolor('red');
+var gf = gf.fontcolor('green');
+var cat = cat.fontcolor('blue');
+
+document.write(gf + cat + me);
+
+3 + 3
