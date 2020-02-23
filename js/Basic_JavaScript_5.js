85e16e7da9b4bad304a725dc726b5e717e9f8031
unknown <smithcolemen@gmail.com>
2020-02-22 17:56:59 -0800

    booleans and stuff

--- js/Basic_JavaScript_5.js
+++ js/Basic_JavaScript_5.js
@@ -17,3 +17,39 @@ function positinity() {
 function negitinity() {
     document.write(-4E400);
 }
+
+function bool_False() {
+    document.write(6 > 15);
+}
+
+function bool_True() {
+    document.write(6 < 15);
+}
+
+console.log(7 + 10);
+
+function coercion() {
+    document.write('100' + 10);
+}
+
+console.log(10 < 5);
+
+function equal_False() {
+    document.write(10 == 10);
+}
+
+function equal_True() {
+    document.write(3 == 10);
+}
+
+function triple_Equal() {
+    var one = 1;
+    var two = 1;
+    var three = 'one';
+    var four = 'two';
+    var five = 5;
+    document.write(one === two);
+    document.write(one === five);
+    document.write(two === three);
+    document.write(three === four);
+}
