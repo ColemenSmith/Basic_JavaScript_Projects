838d95d9d2c34d0a5f7c66c3a5d8faa3e40e9a0a
ColemenSmith <smithcolemen@gmail.com>
2020-02-29 17:51:27 -0800

    Did some conts and let and return

---
+++ js/Basic_JavaScript_10.js
@@ -0,0 +1,52 @@
+function Call_Loop() {
+    var i = 1;
+    var digit = "";
+    while (i < 21) {
+        digit += "<br>" + i;
+        i++;
+    }
+    document.getElementById("Loop").innerHTML = digit;
+}
+
+function strLeng() {
+    var strng = "Let's see how long this is."
+    var n = strng.length;
+    document.getElementById("length").innerHTML = n;
+}
+
+function for_loop() {
+    var myDictionary = ['Guitar', 'Ukelele', 'Trombone', 'Bass', 'Trumpet', 'Clarinet'];
+    var content = '';
+    var i;
+    for (i = 0; i < myDictionary.length; i++) {
+        content += "<br>" + myDictionary[i];
+    }
+    document.getElementById("List_of_Instruments").innerHTML = content;
+}
+
+function array_Function() {
+    var array = [];
+    array[0] = 'running';
+    array[1] = 'swimming';
+    array[2] = 'sleeping';
+    array[3] = 'gaming';
+    document.getElementById('Array').innerHTML = 'On the weekends I like to go ' + array[0] + '.';
+}
+
+function constant_Function() {
+    const food = { type: 'Pizza', toppings: "Pepperoni and bacon", crust_Type: "Deep dish" };
+    food.price = 15;
+    food.cook_Time = 'Thirty minutes';
+    food.meal = 'Dinner';
+    document.getElementById('Constant').innerHTML = 'My favorite food is ' + food.type + ', I prefer ' + food.toppings + ' on it. It usually costs me around ' + food.price + ' dollars, and arrives in ' + food.cook_Time + '. You usually eat it at ' + food.meal + ' time.';
+}
+
+var food = 'Pizza';
+document.write("<br>" + food);
+{
+    let food = 'Sandwich';
+    document.write("<br>" + food);
+}
+document.write("<br>" + food);
+
+
